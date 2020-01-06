1bd2d59a75a5b6d6a8ebb1ef4e41038d3e1da7df
Craig Mackles <36550890+Vulsin@users.noreply.github.com>
2020-01-06 12:23:27 -0500

    Removed redundant source files

--- buildings.asm
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; buildings.asm
-; Sprites and subroutines for the buildings
-;
-; Written by Craig Mackles
-; https://github.com/Vulsin/aphelion-a2600
-;
-; Distributed under the MIT License
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- enemies.asm
+++
@@ -1,9 +0,0 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; enemies.asm
-; Sprites and subroutines for the enemies
-;
-; Written by Craig Mackles
-; https://github.com/Vulsin/aphelion-a2600
-;
-; Distributed under the MIT License
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
